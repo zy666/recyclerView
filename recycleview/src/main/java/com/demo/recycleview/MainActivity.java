1b1b15e84ce05187f2a0e62edf2d2cdffa2ec801
zhouyou <zhouyou.519@gmail.com>
2016-07-16 22:22:38 +0800

    Initial commit

---
+++ recycleview/src/main/java/com/demo/recycleview/MainActivity.java
@@ -0,0 +1,70 @@
+package com.demo.recycleview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<String> mDatas;
+    private HomeAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        mRecyclerView = (RecyclerView) findViewById(R.id.bank_cards);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
+    }
+
+    protected void initData(){
+        mDatas = new ArrayList<String>();
+        for (int i='A';i<'z';i++) {
+            mDatas.add(""+(char)i);
+        }
+    }
+
+    public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>{
+
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.item_list,parent,false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.tv.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder
+        {
+
+            TextView tv;
+
+            public MyViewHolder(View view)
+            {
+                super(view);
+                tv = (TextView) view.findViewById(R.id.id_num);
+            }
+        }
+
+    }
+
+
+}
