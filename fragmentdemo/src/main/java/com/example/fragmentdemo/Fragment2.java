05098f02f512281aaa40b0fe4ae603b07fa08bcb
zhouyou <zhouyou@renwohua.com>
2016-07-20 18:22:28 +0800

    add fragmentDemo

---
+++ fragmentdemo/src/main/java/com/example/fragmentdemo/Fragment2.java
@@ -0,0 +1,39 @@
+package com.example.fragmentdemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment2 extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button button = (Button) getActivity().findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = (TextView) getActivity().findViewById(R.id.fragment1_text);
+//                Toast.makeText(getActivity(), textView.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ fragmentdemo/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.fragmentdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        if (display.getWidth() >display.getHeight()) {
+            Fragment1 fragment1 = new Fragment1();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment1).commit();
+        }else {
+            Fragment2 fragment2 = new Fragment2();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,fragment2).commit();
+        }
+    }
+}
